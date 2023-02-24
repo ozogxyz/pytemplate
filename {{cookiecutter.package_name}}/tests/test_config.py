9b531e2197bb2526595f6a557c04274adfa2c3b7
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-02-24 21:45:30 +0300

    minimalism

--- {{cookiecutter.package_name}}/tests/test_config.py
+++ {{cookiecutter.package_name}}/tests/test_config.py
@@ -1,16 +1,5 @@
-import hydra
-from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
 
 
-def test_train_config(cfg_train: DictConfig):
+def test_config(cfg_train: DictConfig):
     assert cfg_train
-    assert cfg_train.data
-    assert cfg_train.model
-    assert cfg_train.trainer
-
-    HydraConfig().set_config(cfg_train)
-
-    hydra.utils.instantiate(cfg_train.data)
-    hydra.utils.instantiate(cfg_train.model)
-    hydra.utils.instantiate(cfg_train.trainer)
