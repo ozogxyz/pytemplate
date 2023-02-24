5fbad244a0aa7ac754be0773bcfaf639e8cf29a8
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-02-24 21:19:40 +0300

    only train, no eval

--- {{cookiecutter.package_name}}/tests/conftest.py
+++ {{cookiecutter.package_name}}/tests/conftest.py
@@ -1,13 +1,14 @@
 import pytest
 from hydra import compose, initialize
-from omegaconf import DictConfig
+from hydra.core.global_hydra import GlobalHydra
+from omegaconf import DictConfig, open_dict
 
 
 @pytest.fixture(scope="package")
 def cfg_train() -> DictConfig:
     with initialize(version_base="1.2", config_path="../config"):
         cfg = compose(
-            config_name="config.yaml", return_hydra_config=True, overrides=[]
+            config_name="train.yaml", return_hydra_config=True, overrides=[]
         )
 
     return cfg
--- {{cookiecutter.package_name}}/tests/test_config.py
+++ {{cookiecutter.package_name}}/tests/test_config.py
@@ -1,9 +1,16 @@
+import hydra
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
 
 
 def test_train_config(cfg_train: DictConfig):
     assert cfg_train
-    assert cfg_train.paths
+    assert cfg_train.data
+    assert cfg_train.model
+    assert cfg_train.trainer
 
     HydraConfig().set_config(cfg_train)
+
+    hydra.utils.instantiate(cfg_train.data)
+    hydra.utils.instantiate(cfg_train.model)
+    hydra.utils.instantiate(cfg_train.trainer)
