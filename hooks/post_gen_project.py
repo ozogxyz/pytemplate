994c683bb6384e642f57e3bda180d2538a36648b
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-02-25 13:11:48 +0300

    remove pre-commit from post_gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,5 +3,3 @@ import subprocess
 subprocess.call(["git", "init"])
 subprocess.call(["git", "add", "*"])
 subprocess.call(["git", "commit", "-m", "Initial commit"])
-subprocess.call(["pre-commit", "install"])
-subprocess.call(["pre-commit", "run", "--all-files"])
